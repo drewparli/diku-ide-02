f9e7a64ebeab6f3667ce8d9d94ce2dd264b09fb0
Andrew Tristan Parli <drewparli@gmail.com>
2017-11-30 13:58:44 +0100

    normal update

--- main.js
+++ main.js
@@ -27,12 +27,45 @@ function main()
 }
 
 // set constants
-var h = 400                       // overall height
-var w = 1200                       // overall width
+var h = 600                       // overall height
+var w = 800                       // overall width
 var origin = {"x": 50, "y": 200}  // temperature graph
+var xScale;
+var yScale;
+
 
 function initGraph()
 {
+
+  var tmax = d3.max(dataset, function(d)
+  {
+    return Math.max.apply(null, d.temps)
+  })
+
+  var tmin = d3.min(dataset, function(d)
+  {
+    return Math.min.apply(null, d.temps)
+  })
+
+  console.log("MinMax", tmin, tmax)
+
+  // Insipration for using scales - https://bl.ocks.org/mbostock/3371592
+
+  xScale = d3.scaleLinear()
+    .domain([0,11])   // this is the value on the axis
+    .range([0, w])  // this is the space allocated the axis
+    .nice();
+
+  yScale = d3.scaleLinear()
+    .domain([-20, 45])   // this is the value on the axis
+    .range([h, 0])  // this is the space allocated the axis
+    .nice();
+
+  var xAxis = d3.axisBottom(xScale)
+    .ticks(10)
+
+  var yAxis = d3.axisLeft(yScale)
+    .ticks(10)
 
 
   //initiate the svg graph object
@@ -44,32 +77,41 @@ function initGraph()
   // console.log("svg", svg)
 
   svg.append("svg:g")
+    .attr("class", "axis")
+    .attr("fill", "red")
+    .call(xAxis)
+
+  svg.append("svg:g")
+    .attr("class", "axis")
+    .call(yAxis)
+
+  svg.append("svg:g")
     .attr("id", "tempgraph")
 
   var tempgraph = d3.select("#tempgraph")
 
   // make the axes
-  tempgraph.append("svg:g")
-    .attr("id", "x-axis")
-    .attr("class", "axis")
+  // tempgraph.append("svg:g")
+  //   .attr("id", "x-axis")
+  //   .attr("class", "axis")
 
-  tempgraph.append("svg:g")
-    .attr("id", "y-axis")
-    .attr("class", "axis")
+  // tempgraph.append("svg:g")
+  //   .attr("id", "y-axis")
+  //   .attr("class", "axis")
 
-  var ax = d3.select("#x-axis")
-    .append("svg:line")
-    .attr("x1", origin.x)
-    .attr("x2", origin.x + 300)
-    .attr("y1", origin.y)
-    .attr("y2", origin.y)
+  // var ax = d3.select("#x-axis")
+  //   .append("svg:line")
+  //   .attr("x1", origin.x)
+  //   .attr("x2", origin.x + 300)
+  //   .attr("y1", origin.y)
+  //   .attr("y2", origin.y)
 
-  var ay = d3.select("#y-axis")
-    .append("svg:line")
-    .attr("x1", origin.x)
-    .attr("x2", origin.x)
-    .attr("y1", origin.y)
-    .attr("y2", origin.y - 120)
+  // var ay = d3.select("#y-axis")
+  //   .append("svg:line")
+  //   .attr("x1", origin.x)
+  //   .attr("x2", origin.x)
+  //   .attr("y1", origin.y)
+  //   .attr("y2", origin.y - 120)
 }
 
 
@@ -92,7 +134,7 @@ var lines = function(dataset)
         {
           // still need to handle missing data
           // represented as value 999.9
-          return "" + (origin.x + (i*100)) + "," + (origin.y - (t*7))
+          return "" + xScale(i) + "," + yScale(t)
         })
       console.log(pairs)
       return pairs.join(" ")
